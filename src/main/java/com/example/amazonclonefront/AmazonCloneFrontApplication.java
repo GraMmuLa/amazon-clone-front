fee2ddb864d8af5ba9828d3521a26e7586b97ee8
GraMmuLa <poliferik@gmail.com>
2024-03-17 14:15:08 +0100

    Initialized project

---
+++ src/main/java/com/example/amazonclonefront/AmazonCloneFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.amazonclonefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonCloneFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonCloneFrontApplication.class, args);
+	}
+
+}
